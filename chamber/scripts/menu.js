dfbb2e263e1f7dd5cb8e134d636637debb792390
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2023-10-11 00:31:05 -0600

    removing comments for the audit...

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,14 +1,9 @@
 /* Define my constants using DOM */
-//const navMenu = document.querySelector('.menu-list')
 const navContainer = document.querySelector('.navigation');
 const hamburger = document.querySelector('#hamburger');
 const main=document.querySelector('main');
 const logo=document.getElementById('logo-img')
-/*const logo=document.querySelector('.logo');  
-const header=document.querySelector('header');
-const footer=document.querySelector('footer');
-*/
-/* Using event listener, toggle the 'show' option for the nav and hamburger menus */
+
 hamburger.addEventListener('click', () => {
 	//navMenu.classList.toggle('show');
 	hamburger.classList.toggle('show');
