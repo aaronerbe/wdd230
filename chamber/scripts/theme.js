1522923e3fde686b26f66a48e32de036324d021a
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2023-10-12 21:37:43 -0600

    change dark/light icons to svg

--- chamber/scripts/theme.js
+++ chamber/scripts/theme.js
@@ -1,26 +1,23 @@
-const sun = "images/white-sun.png";
-const moon = "images/moon.png"
+//const sun = "images/white-sun.png";
+const sun = "images/sun-white.svg"
+//const moon = "images/moon.png"
+const moon = "images/moon-white.svg"
 
 var theme = "light";
 const root = document.querySelector(":root");
 const container = document.getElementsByClassName("theme-container")[0];
 const themeIcon = document.getElementById("theme-icon");
 container.addEventListener("click", setTheme);
-console.log(theme);
 
   function setTheme() {
     switch (theme) {
       case "dark":
-        console.log("running setLight");
         setLight();
         theme = "light";
-        console.log(theme)
         break;
       case "light":
-        console.log("running setDark");
         setDark();
         theme = "dark";
-        console.log(theme)
         break;
     }
   }
@@ -42,7 +39,7 @@ console.log(theme);
     root.style.setProperty("--themed-background","var(--dark-accent-color)");
     root.style.setProperty("--themed-text","var(--secondary-color)");
     root.style.setProperty("--themed-highlight-background", "var(--dark-accent-color)")
-    console.log(root.style)
+    //console.log(root.style)
     container.classList.remove("shadow-light");
     setTimeout(() => {
       container.classList.add("shadow-dark");
