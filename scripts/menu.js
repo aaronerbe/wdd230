a822e07fcd4eb9e79af5f2d9a2944efb0ec55b0b
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2023-10-24 22:34:03 -0600

    adjusting to make the audit happy  no other reason

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,7 +3,7 @@ const navMenu = document.querySelector('.navigation')
 const hamburger = document.querySelector('#menu');
 const header = document.querySelector('header');
 const mainCont = document.querySelector('.main');
-const footer = document.querySelector('.footer');
+const footer = document.querySelector('footer');
 
 /* Using event listener, toggle the 'show' option for the nav and hamburger menus */
 hamburger.addEventListener('click', () => {
