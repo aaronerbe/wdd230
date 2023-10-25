7f1417cb1f42c869e2a04db9360dbff42c08a703
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2023-10-24 22:29:25 -0600

    fix some web accessibility stuff

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,16 @@
 /* Define my constants using DOM */
 const navMenu = document.querySelector('.navigation')
 const hamburger = document.querySelector('#menu');
-//const header = document.querySelector('header');
+const header = document.querySelector('header');
 const mainCont = document.querySelector('.main');
+const footer = document.querySelector('.footer');
 
 /* Using event listener, toggle the 'show' option for the nav and hamburger menus */
 hamburger.addEventListener('click', () => {
 	navMenu.classList.toggle('show');
 	hamburger.classList.toggle('show');
 	mainCont.classList.toggle('show');
+	header.classList.toggle('show');
+	footer.classList.toggle('show');
 	console.log('toggle');
 });
