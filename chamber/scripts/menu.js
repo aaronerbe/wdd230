8fbcd062cd2f9f390b91056301f1e7118f779cb7
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2023-10-26 01:41:44 -0600

    main backbones of discovery page started.

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -16,3 +16,21 @@ hamburger.addEventListener('click', () => {
 		logo.src="images/small-brown-logo-transparent.webp"
 	}
 });
+
+
+let portrait = window.matchMedia("(orientation: portrait)");
+
+portrait.addEventListener("change", function(e) {
+	//if it changes close the menu (if it's open) so it doesn't get caught in weird state where buttons aren't visible
+	//close the menu by 'clicking' the button which kicks off the function above to toggle all the elements
+	if(e.matches) {
+        // Portrait mode
+		console.log("portrait");
+    } else {
+        // Landscape
+		if (hamburger.classList.contains('show')){
+			hamburger.click();
+		}
+		console.log("landscape");
+    }
+})
---
+++ chamber/scripts/visits.js
@@ -0,0 +1,32 @@
+const visitContent = document.querySelector('.visit-content');
+const today = Date.now();
+const msToDays = 84600000;
+
+function getVisitDate(){
+    return JSON.parse(localStorage.getItem("lastVisitDate"));
+}
+function setVisitDate(today){
+    localStorage.setItem("lastVisitDate", today);
+}
+
+
+let lastDate = getVisitDate() || today;
+//used to force test > 1 day visit
+//lastDate-=(5*msToDays);
+
+// Calculate the time difference between today and the last visit in days.
+const daysDifference = (today - lastDate) / msToDays;
+
+if (today == lastDate) {
+    console.log("First visit.");
+    //do nothing to the visitsContent as it's correct by default
+} else if (daysDifference <1) {
+    console.log("Less than a day since the last visit.");
+    visitContent.textContent = "Back so soon! Awesome!";
+} else {
+    console.log("More than 1 day since the last visit.");
+    visitContent.textContent = "You last visited " + parseInt(daysDifference) + " ago.";
+}
+
+//set for next visit
+setVisitDate(today);
