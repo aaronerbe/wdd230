a1d147e38abbe6d30a90502d72a258e1745a1b87
aaronerbe <58095346+aaronerbe@users.noreply.github.com>
2023-12-13 18:57:14 -0700

    add an alert for end date before start date.  Still need to prevent start before tday

--- scoots/scripts/reservations.js
+++ scoots/scripts/reservations.js
@@ -1,13 +1,6 @@
-//Auto calculate the perio based on dates entered.
-//Update end date if beginning date exists 
-//zero out period of end date is before start date (negative period) preventing them to submit since it's required field
-//also zero out if start date is in past
-//need to work on warning messages when they do these...
-
 const startDateInput = document.getElementById('reservation-start');
 const endDateInput = document.getElementById('reservation-end');
 const periodInput = document.getElementById('period');
-const today = new Date();
 
 startDateInput.addEventListener('change', updateReservationDuration);
 endDateInput.addEventListener('change', updateReservationDuration);
@@ -18,31 +11,29 @@ function updateReservationDuration() {
     let endDate = new Date(endDateInput.value);
     
     // Check if dates are valid.  Required to wait until both dates are populated
-    //console.log(today)
-    if (!isNaN(startDate.getTime()) && !isNaN(endDate.getTime()) && (startDate.getTime() > today.getTime())){
+    if (!isNaN(startDate.getTime()) && !isNaN(endDate.getTime())){
         // Calculate the difference in milliseconds, then convert ms to days
         let difference = endDate - startDate;
         let durationInDays = difference / (1000 * 60 * 60 * 24);
         
         // Update the periodInput value with the calculated duration (rounded to nearest whole number)
-        if (!isNaN(durationInDays)) {
-            if(durationInDays>0){
-                periodInput.value = Math.round(durationInDays);
-            }
-            else{periodInput.value = '';} // Clear the value if the duration is NaN
-        } 
-        else {
+        if(durationInDays>0){
+            periodInput.value = Math.round(durationInDays);
+        }
+            // Clear the value if the duration <=0
+        else{
+            alert('End Date must be afte Start Date');
             periodInput.value = '';
-            endDateInput.value = '';
-        } // Clear the value if the duration is NaN
+        } 
     } 
     else {
+        //resets things if end and period get out of sync
         periodInput.value = '';
         endDateInput.value = '';
-    } // Clear the value if dates are invalid
+    }
 }
 
-
+//update the End Date based on input from the period field
 function updateEndDateFromPeriod() {
     const startDate = new Date(startDateInput.value);
     const period = parseInt(periodInput.value);
@@ -50,7 +41,6 @@ function updateEndDateFromPeriod() {
     if (!isNaN(startDate.getTime()) && !isNaN(period) && startDate > today) {
         const endDate = new Date(startDate);
         endDate.setDate(startDate.getDate() + period);
-
         endDateInput.value = endDate.toISOString().split('T')[0];
     }else{
         periodInput.value = ''; // Clear the value if dates are invalid
